d561e3acce5631cb6d970a52ebd3c121fb531a05
Kevin Gilbert <kgilbert.cmu@gmail.com>
2014-03-06 14:50:49 -0500

    basically created an enron email search engine that returns the words most distinctive with a set of input query words. Subset corpus then return max tfidf colsums,

--- src/code.R
+++ src/code.R
@@ -19,3 +19,29 @@ docify = function(document_number) {
 docs = lapply(1:39861, docify)
 corp = Corpus(VectorSource(docs))
 dtm_test = DocumentTermMatrix(corp)
+
+library(parallel)
+library(tm)
+library(slam)
+library(skmeans)
+clist = list(weighting=weightTfIdf, stemming=T, removeNumbers=T, removePunctuation=T)
+dtm = DocumentTermMatrix(enronCorpus,control=clist)
+save(dtm, file="dtm.RData")
+
+wordList = c("skill", "legal", "rick", "meet")
+x = findAssocs(dtm,wordList, rep(.1, length(wordList)))
+write(x, "wordAssocs.txt", append=T)
+
+get = function(wordList,dtm) {
+    i = dtm[,stemDocument(wordList)]$i
+    rows = unique(i[which(table(i) > 1)])
+    return(rows)
+}
+
+redoc = function(docList) {
+    return(DocumentTermMatrix(enronCorpus[docList], clist))
+}
+
+words = function(num, wordList) {
+    return(rev(sort(col_sums(redoc(get(wordList, dtm)))))[1:num])
+}
